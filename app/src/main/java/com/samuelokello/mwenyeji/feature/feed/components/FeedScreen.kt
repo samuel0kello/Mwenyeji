e88aa6ad757834e00263f99fd7cee3ef10736f18
Samuel Okello <samuel.okello@mouvafrica.com>
2026-03-28 21:40:53 +0300

    update dependencies and project configuration

--- app/src/main/java/com/samuelokello/mwenyeji/feature/feed/components/FeedScreen.kt
+++ app/src/main/java/com/samuelokello/mwenyeji/feature/feed/components/FeedScreen.kt
@@ -1,13 +1,16 @@
 package com.samuelokello.mwenyeji.feature.feed.components
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import com.samuelokello.mwenyeji.ui.designsystem.components.MwenyejiHeader
+import com.samuelokello.mwenyeji.ui.designsystem.components.card.MwenyejiCard
 
 @Composable
 fun FeedScreen(modifier: Modifier = Modifier) {
@@ -37,12 +40,57 @@ internal fun FeedScreenContent(modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun LocationInput(modifier: Modifier = Modifier) {
+fun RouteSearchCard(modifier: Modifier = Modifier) {
+    MwenyejiCard(
+
+    ) {
+        Column() {
+
+        }
+    }
+}
+
+
+@Composable
+fun LocationField(
+    modifier: Modifier = Modifier,
+    icon: ImageVector,
+    placeHolder: String,
+    trailingChevron: ImageVector
+) {
     TextField(
         state =
     )
 }
 
+@Composable
+fun SectionHeader(
+    title: String,
+    modifier: Modifier = Modifier
+) {
+
+}
+
+@Composable
+fun TimeOfDaySelector(
+    modifier: Modifier = Modifier,
+    selected: TimeOfDay,
+    onSelected: (TimeOfDay) -> Unit
+) {
+
+}
+
+@Composable
+fun TimeOfDayChip(modifier: Modifier = Modifier) {
+
+}
+
+
+enum class TimeOfDay {
+    MORNING,
+    MIDDAY,
+    EVENING
+}
 
 @Preview
 @Composable
